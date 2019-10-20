772064da0ce66443e89de5c00a3a0752e1b146e1
EvgeniyKoch <evgkochz@gmail.com>
2019-10-20 21:16:40 +0300

    Updates

---
+++ precache-manifest.3174ea4a2daa0d1034a3d3b8466e869b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ddb8377f1c86ba3b62925373d2de699",
+    "url": "/PhoneShop/index.html"
+  },
+  {
+    "revision": "b289c051176145da7cc4",
+    "url": "/PhoneShop/static/css/main.812602da.chunk.css"
+  },
+  {
+    "revision": "d019c5fa5c38f61976a4",
+    "url": "/PhoneShop/static/js/2.eeda97d9.chunk.js"
+  },
+  {
+    "revision": "b289c051176145da7cc4",
+    "url": "/PhoneShop/static/js/main.49ce799b.chunk.js"
+  },
+  {
+    "revision": "ff12e39151d8dcfe124f",
+    "url": "/PhoneShop/static/js/runtime-main.f4714e96.js"
+  }
+]);
